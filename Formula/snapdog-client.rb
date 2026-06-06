2ac325c6e4b3ad242e33a02233fffa2c8f9015e1
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2026-06-06 08:31:16 +0200

    Add SnapDog Homebrew formulas

---
+++ Formula/snapdog-client.rb
@@ -0,0 +1,23 @@
+class SnapdogClient < Formula
+  desc "SnapDog multiroom audio client"
+  homepage "https://github.com/SnapDogRocks/snapdog"
+  license "GPL-3.0-only"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/SnapDogRocks/snapdog/releases/download/v0.16.1/snapdog-v0.16.1-x86_64-apple-darwin.tar.gz"
+      sha256 "1f3309501abadeb32373da3ecbae2e7ee0e6342379096563b5289e4b69e5e03f"
+    else
+      url "https://github.com/SnapDogRocks/snapdog/releases/download/v0.16.1/snapdog-v0.16.1-aarch64-apple-darwin.tar.gz"
+      sha256 "e704b83bf908d43f3c43329a4e12d230add6298918c27f9b4de63c030364522c"
+    end
+  end
+
+  def install
+    bin.install "snapdog-client"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/snapdog-client --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,33 @@
+# SnapDog Homebrew Tap
+
+Homebrew formulas for SnapDog server and client releases.
+
+## Installation
+
+```bash
+brew tap snapdogrocks/tap
+brew install snapdog
+brew install snapdog-client
+```
+
+You can also install without tapping first:
+
+```bash
+brew install snapdogrocks/tap/snapdog
+brew install snapdogrocks/tap/snapdog-client
+```
+
+## Formulas
+
+### snapdog
+
+Multi-zone audio controller with AirPlay, Snapcast, MQTT, and KNX.
+
+### snapdog-client
+
+SnapDog multiroom audio client.
+
+## Releases
+
+Formula updates are published by the SnapDog release workflow in
+[SnapDogRocks/snapdog](https://github.com/SnapDogRocks/snapdog).
